709e46b38ec356ef7d5a0e499e6ba21b90032b0f
Alvaro <aandresalvarez@gmail.edu>
2019-03-15 14:21:13 -0700

    initial2

--- HelpResources.php
+++ HelpResources.php
@@ -5,48 +5,25 @@ namespace Stanford\HelpResources;
 class HelpResources extends \ExternalModules\AbstractExternalModule {
 	  function __construct() {
 		parent::__construct();
+         // System::init();
+         //$lang['home_62']="testtttt";
 		// Other code to run when object is instantiated
 	}
     function redcap_every_page_top($project_id)
     {
-        if (isset($_GET['action']) == 'training') {
-
+        if (isset($_GET['action']) && $_GET['action'] == 'training') {
            $resources_page= $this->getUrl("index.html");
-
             ?>
-
-
             <script>
-
-
                 $(document).ready(function() {
-
                     var resources_pag="<?php echo  $resources_page; ?>";
-
-                    // $('ul.nav a.active').append('<b class="caret"></b>');
-                   $('#pagecontent').prepend("<div id='em-info' ></div>");
-
-                     $('#em-info').load(resources_pag);
-
-
-                });            </script>
-
-
-
-
+                    $('#nav-tab-training a').html("<i class='fas fa-map-signs'></i> Resources & Training"); //redcap variable  $lang['home_62']
+                    $('#pagecontent').prepend("<div id='em-info' ></div>");
+                    $('#em-info').load(resources_pag);
+                });
+            </script>
             <?php
-
         }
-
-
-
-
-
-
     }
-
-
-
-
 
 }
